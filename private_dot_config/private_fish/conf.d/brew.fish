634bca874ccdbe17d62ac47ba6c341ab4294c2ae
Noah Kempers <n.kempers@westfalen.com>
2024-08-20 15:18:44 +0200

    add brew completions

--- private_dot_config/private_fish/conf.d/brew.fish
+++ private_dot_config/private_fish/conf.d/brew.fish
@@ -10,3 +10,12 @@ end
 fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin";
 ! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH;
 ! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH;
+
+
+if test -d (brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
